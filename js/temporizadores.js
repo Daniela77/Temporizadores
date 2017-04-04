f37ff43b119d8a7f459f32535edbdd91bd4344bd
Daniela77 <danielacolamai@gmail.com>
2017-04-04 12:04:12 -0300

    Correcciones Uses strict y funcion setInterval

--- js/temporizadores.js
+++ js/temporizadores.js
@@ -1,39 +1,38 @@
-$(document).ready(function(){
-
-    $("button").on("click", function(){
-       event.preventDefault();
-       var temporizadores = cargarTemporizadores();
-       lanzarTemporizadores(temporizadores);
-    });
+$(document).ready(function () {
+    'use strict';
 
     function cargarTemporizadores() {
-      var temporizadores=[];
-      var cantTemp = $("#inputTemp").val();
-      $("#inputTemp").val("");
-      for (i=0; i <cantTemp ; i++) {
-        var segundos=Math.floor((Math.random() * 5)+1);
-        var inicTemp= segundos*1000;
-        temporizadores.push(inicTemp);
-      }
-      return temporizadores;
+        var temporizadores, cantTemp, i, segundos, inicTemp;
+        temporizadores = [];
+        cantTemp = $("#inputTemp").val();
+        $("#inputTemp").val("");
+        for (i = 0; i < cantTemp; i++) {
+            segundos = Math.floor((Math.random() * 5) + 1);
+            inicTemp = segundos * 1000;
+            temporizadores.push(inicTemp);
+        }
+        return temporizadores;
+    }
+
+    function crearBoton(nombre) {
+        $("body").append("<button id=tempbutton" + nombre + ">Boton " + nombre + "</button>");
+        $("#tempbutton" + nombre).on("click", function () {
+            alert("Hola soy el boton: " + nombre);
+        });
     }
 
     function lanzarTemporizadores(temporizadores) {
-      for ( i = 0; i < temporizadores.length; i++) {
-        var tiempoinicial= temporizadores[i];
-        activarTimer(tiempoinicial, i);
-      }
+        var i, tiempoinicial;
+        for (i = 0; i < temporizadores.length; i++) {
+            tiempoinicial = temporizadores[i];
+            setTimeout(crearBoton, tiempoinicial, i);
+        }
     }
 
-    function activarTimer(segundos, nombre){
-      setTimeout(function(){crearBoton(nombre);},segundos);
-    }
-
-    function crearBoton(nombre){
-      $("body").append("<button id='tempbutton"+nombre+"'>Boton "+nombre+"</button>");
-      $("#tempbutton"+nombre).on("click", function(){
-         alert("Hola soy el boton: " + nombre);
-      });
-    }
+    $("button").on("click", function () {
+        event.preventDefault();
+        var temporizadores = cargarTemporizadores();
+        lanzarTemporizadores(temporizadores);
+    });
 
 });
